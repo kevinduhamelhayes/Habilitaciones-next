16f6fe14b365fe4eee8e1760cba768208729f322
Ianduha13 <ianduhamelhayes@gmail.com>
2023-09-17 01:18:00 -0300

    fix:swiper fix

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,7 @@ import Swipper from '@/components/Swipper';
 export default function Home() {
   return (
     <NextUIProvider>
-      <main className="max-w-full overflow-x-hidden relative flex flex-col items-center h-full bg-slate-200 ">
+      <main className="max-w-full relative flex flex-col items-center h-full bg-slate-200 ">
         <Navbar />
         <Hero />
         <Cards />
--- components/Swipper.tsx
+++ components/Swipper.tsx
@@ -10,26 +10,54 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 
 const Swipper = () => {
   return (
-    <section className='h-[800px] max-h-[800px]'>
-    <Swiper
-      modules={[Navigation, Pagination, Scrollbar, A11y]}
-      spaceBetween={50}
-      loop={true}
-      slidesPerView={3}
-      navigation
-      pagination={{ clickable: true }}
-      scrollbar={{ draggable: true }}
-      onSwiper={(swiper) => console.log(swiper)}
-      onSlideChange={() => console.log('slide change')}
-    >
-      <SwiperSlide><Image src="/obras1.jpg" alt="obra1" width={1400} height={800} className='mx-auto h-full object-cover lg:object-contain'/></SwiperSlide>
-      <SwiperSlide><Image src="/obras2.jpg" alt="obra2" width={1400} height={800} className='mx-auto h-full object-cover lg:object-contain'/></SwiperSlide>
-      <SwiperSlide><Image src="/obras3.jpg" alt="obra3" width={1400} height={800} className='mx-auto h-full object-cover lg:object-contain'/></SwiperSlide>
-      <SwiperSlide><Image src="/obras4.jpeg" alt="obra4" width={1400} height={800} className='mx-auto h-full object-cover lg:object-contain'/></SwiperSlide>
-      <SwiperSlide><Image src="/obras5.jpg" alt="obra5" width={1400} height={800} className='mx-auto h-full object-cover lg:object-contain'/></SwiperSlide>
-      <SwiperSlide><Image src="/obras6.jpg" alt="obra6" width={1400} height={800} className='mx-auto h-full object-cover lg:object-contain'/></SwiperSlide>
-      <SwiperSlide><Image src="/obras7.jpg" alt="obra7" width={1400} height={800} className='mx-auto h-full object-cover lg:object-contain'/></SwiperSlide>
-    </Swiper>
+    <section className=' w-full overflow-x-hidden h-[600px] flex items-center overflow-y-hidden'>
+        <Swiper
+          modules={[Navigation, Pagination]}
+          spaceBetween={50}
+          slidesPerView={2}
+          className='h-[600px] w-full'
+          navigation
+          pagination={{ clickable: true }}
+          scrollbar={{ draggable: true }}
+          onSwiper={(swiper) => console.log(swiper)}
+          onSlideChange={() => console.log('slide change')}
+        >
+        <SwiperSlide>
+        <figure className='w-full h-[600px]'>
+          <Image src="/obras1.jpg" alt="obra1" width={1400} height={800} className='mx-auto h-full object-contain '/>
+        </figure>
+        </SwiperSlide>
+        <SwiperSlide>
+        <figure className='w-full h-'>
+          <Image src="/obras2.jpg" alt="obra1" width={1400} height={800} className='mx-auto h-full object-contain '/>
+        </figure>
+        </SwiperSlide>
+        <SwiperSlide>
+        <figure className='w-full h-[600px]'>
+          <Image src="/obras3.jpg" alt="obra1" width={1400} height={800} className='mx-auto h-full object-contain '/>
+        </figure>
+        </SwiperSlide>
+        <SwiperSlide>
+        <figure className='w-full h-[600px]'>
+          <Image src="/obras4.jpg" alt="obra1" width={1400} height={800} className='mx-auto h-full object-contain '/>
+        </figure>
+        </SwiperSlide>
+        <SwiperSlide>
+        <figure className='w-full h-[600px]'>
+          <Image src="/obras5.jpg" alt="obra1" width={1400} height={800} className='mx-auto h-full object-contain '/>
+        </figure>
+        </SwiperSlide>
+        <SwiperSlide>
+        <figure className='w-full h-[600px]'>
+          <Image src="/obras6.jpg" alt="obra1" width={1400} height={800} className='mx-auto h-full object-contain '/>
+        </figure>
+        </SwiperSlide>
+        <SwiperSlide>
+        <figure  className='w-full h-[600px]'>
+          <Image src="/obras7.jpg" alt="obra1" width={1400} height={800} className='mx-auto h-full object-contain '/>
+        </figure>
+        </SwiperSlide>
+   </Swiper>
     </section>
   );
 };
