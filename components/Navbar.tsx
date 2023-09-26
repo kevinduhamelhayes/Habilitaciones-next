9ffbe9af9ac29eabfbc1639c24f82bbf780ebd26
Kevin Duhamel Hayes <kevinduhamelh@gmail.com>
2023-09-26 17:29:08 -0300

    no anda

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,44 +1,53 @@
 import React from "react";
-import {Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button} from "@nextui-org/react";
-import {AcmeLogo} from "./AcmeLogo.jsx";
-import {SearchIcon} from "./SearchIcon.jsx";
+import { Link, Button } from "@nextui-org/react";
+import { AcmeLogo } from "./AcmeLogo";
 
-const Component = () => {
-
+const Navbar = () => {
   return (
-    <Navbar className="bg-black rounded-b-xl min-h-[90px] text-white" position="sticky">
-      <NavbarBrand>
-        <AcmeLogo />
-        <div className="hidden md:flex">
-        <p className="font-bold text-2xl">Habilitaciones Rosario</p>
+    <div className="bg-black w-full rounded-b-xl min-h-[90px] text-white flex justify-between items-center p-4 sticky top-0 z-50">
+      <div className="flex flex-row items-center justify-evenly max-w-[90%] w-full">
+        <div className="flex items-center">
+          
+          <div className="mr-4">
+          <AcmeLogo/>
+          </div>
+          <div className=" md:flex">
+            <p className="font-bold text-2xl">Habilitaciones Rosario</p>
+          </div>
         </div>
-      </NavbarBrand>
-      <NavbarContent className=" gap-6 ml-12 md:flex hidden " justify="center"> {/* md:hidden esconde el contenido en pantallas medianas y más pequeñas */}
-        <NavbarItem>
-          <Link color="foreground" href="#servicios" className="text-xl font-semibold text-white">
-            Servicios
-          </Link>
-        </NavbarItem>
-        <NavbarItem isActive>
-          <Link href="#"  className="text-lg text-white">
-            Preguntas Frecuentes
-          </Link>
-        </NavbarItem>
-        <NavbarItem>
-          <Link color="foreground" href="#" className="text-lg text-white">
-            Regularizaciones
-          </Link>
-        </NavbarItem>
-      </NavbarContent>
-      <NavbarContent justify="end">
-        <NavbarItem>
-          <Button as="a" color="success" className="text-xl text-white font-semibold px-8 py-6 mx-8" href="https://api.whatsapp.com/send/?phone=%2B543415883040" variant="shadow">
+        <div className="gap-6 ml-12 md:flex  hidden sm:hidden lg:flex xl:flex">
+          <span className="text-xl font-semibold text-white cursor-pointer">
+            <Link color="foreground" href="#servicios">
+              Servicios
+            </Link>
+          </span>
+          <span className="text-lg text-white cursor-pointer">
+            <Link href="#">
+              Preguntas Frecuentes
+            </Link>
+          </span>
+          <span className="text-lg text-white cursor-pointer">
+            <Link color="foreground" href="#">
+              Regularizaciones
+            </Link>
+          </span>
+        </div>
+        <div>
+          <Button
+            as="a"
+            color="success"
+            className="text-xl text-white font-semibold px-8 py-6 mx-8"
+            href="https://api.whatsapp.com/send/?phone=%2B543415883040"
+            variant="shadow"
+          >
             Wats App
           </Button>
-   </NavbarItem>
-      </NavbarContent>
-    </Navbar>
+        </div>
+      </div>
+    </div>
   );
-}
-export default Component;
+};
+
+export default Navbar;
+
 
