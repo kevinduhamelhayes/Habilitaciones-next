542ae7385a39ff2c263842ced9b62378ec69d55a
kevinduhamelhayes <kevinduhamelh@gmail.com>
2023-10-10 17:09:07 -0300

    fix deploy sections

--- components/Section.tsx
+++ components/Section.tsx
@@ -5,15 +5,15 @@ const Section = () => {
         <section className="w-full flex flex-col items-center bg-gray-100 text-black py-20 md:py-32">
             <h2 className="text-4xl md:text-5xl font-bold mb-8">Contáctanos</h2>
             
-            <div className="flex flex-col md:flex-row gap-8 max-w-4xl">
-                <div className="flex flex-col items-center md:items-start">
-                    <h3 className="text-2xl md:text-3xl font-semibold mb-5">Presupuestos</h3>
-                    <p className="text-lg md:text-xl">Calle pte Roca 123</p>
+            <div className="flex flex-col md:flex-row gap-8 max-w-4xl text-center items-center justify-center">
+                <div className="hidden flex flex-col items-center md:items-start">
+                    <h3 className="text-2xl md:text-3xl font-semibold mb-5"></h3>
+                    
                     <p className="text-lg md:text-xl">Rosario, Santa Fe</p>
                     <p className="text-lg md:text-xl">Argentina, CP 2000</p>
                 </div>
 
-                <div className="flex flex-col items-center md:items-start">
+                <div className="flex flex-col items-center gap-4">
                     <h3 className="text-2xl md:text-3xl font-semibold mb-5">Datos de contacto</h3>
                     <p className="text-lg md:text-xl"><span className="font-semibold ">Teléfono:</span> +54 341 588 3040</p>
                     <span className="flex items-center space-x-4"><span className="font-bold text-lg md:text-xl ">Email:  </span> 
